ddfd3db2174c2e428f08d6df74874a40e62c9d5b
Kevin Gager <kevin.gager15@gmail.com>
2019-11-18 13:16:29 -0800

    Removed bug for poly in which arr[0] is not tranlating correctly

--- assignment05/Riemann.java
+++ assignment05/Riemann.java
@@ -86,9 +86,12 @@ public class Riemann{
     for (int i=0;i<numOfRectangles;i++ ) {
       xValue = getXvalue(rectangleWidth);
       for (int j=0;j<arr.length ;j++ ) {
-        if(!(arr[0]==0&&j==0))
+        if(j!=0)
         {
             curRieVal += rectangleWidth* xValue*Math.pow(arr[j],j);
+        }
+        else{
+          curRieVal += rectangleWidth* arr[j];
         }
       }
     }
